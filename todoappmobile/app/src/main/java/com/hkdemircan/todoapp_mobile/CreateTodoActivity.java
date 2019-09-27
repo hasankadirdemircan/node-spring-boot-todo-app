faef18596f7f6c5dd2717b0df7812e820a51953f
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-09-27 22:08:56 +0300

    create todo design and define

--- todoappmobile/app/src/main/java/com/hkdemircan/todoapp_mobile/CreateTodoActivity.java
+++ todoappmobile/app/src/main/java/com/hkdemircan/todoapp_mobile/CreateTodoActivity.java
@@ -3,12 +3,32 @@ package com.hkdemircan.todoapp_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateTodoActivity extends AppCompatActivity {
+
+    EditText todoHeaderEditText, todoContentEditText;
+    Button saveTodoButton;
+
+    //session durumu
+    SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_todo);
+
+        define();
+    }
+
+    private void define(){
+        todoHeaderEditText = findViewById(R.id.todoHeaderEditText);
+        todoContentEditText = findViewById(R.id.todoContentEditText);
+
+        saveTodoButton = findViewById(R.id.saveTodoButton);
+
+        // Session Manager tanımladık.
+        session = new SessionManager(getApplicationContext());
     }
 }
